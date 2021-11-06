347e0f6f5682f5fb39db54efcfc6d4f728dc937b
ramadhansur <ramadhansuryanto44@gmail.com>
2021-11-06 22:33:37 +0700

    first commit

---
+++ app/src/main/java/com/ramadhan/tugas5_alarm/AlarmReceiver.java
@@ -0,0 +1,20 @@
+package com.ramadhan.tugas5_alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    MediaPlayer player;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationHelper notificationHelper = new NotificationHelper(context);
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification();
+        notificationHelper.getManager().notify(1, nb.build());
+    }
+}
